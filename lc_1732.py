17b2bfed983763eeed13e68b26c47125f8b3342b
Garvit Tyagi <gar27vit2005@gmail.com>
2026-06-22 19:09:39 +0530

    Implement largestAltitude method in Solution class

---
+++ lc_1732.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans=0
+        curr=0
+
+        for i in range(len(gain)):
+            curr+=gain[i]
+            ans=max(ans,curr)
+        
+        return ans
